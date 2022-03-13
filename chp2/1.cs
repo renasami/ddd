83fb2d289aa06f412d3e53d0ac807937a23ac31c
renasami <drrr0502@gmail.com>
2022-03-13 15:22:11 +0900

    f

---
+++ chp2/1.cs
@@ -0,0 +1,11 @@
+var fullName = "naruse masanobu"
+Console.WriteLine(fullName);
+
+var tokens = fullName.Split;(' ');
+var lastName = tokens[0];
+Console.WriteLine(lastName);
+
+var fullName = "john smith";
+var tokens = fullName.Split;(' ');
+var lastName = tokens[0]
+Console.WriteLine(lastName);
